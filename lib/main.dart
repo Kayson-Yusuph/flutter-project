76eb8d28d4d1630ddff52b46461264f4d77a6afc
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-05-12 08:18:04 +0300

    Adding a not working image adding functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,43 @@
 import 'package:flutter/material.dart';
-main() {
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
 }
+class _MyAppState extends State<MyApp>{
+  List<String> _products = ['assets/food.jpg'];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    _products.add('assets/food.jpg')
+                  });
+                },
+                child: Text('Add Product'),
+              ),
+            ),
 
-class MyApp extends StatelessWidget{
 
+            Column(children: _products.map((product) => Card(child: Column(children: [Image.asset(product), Text('Food Paradise'),]),)).toList(),
+            ),
+
+
+        ),
+      ),
+    );
+  }
 }
