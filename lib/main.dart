38c4171dc2007122ab227fd3516cdaacd5718f0b
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-05-14 19:15:16 +0300

    Defining application themes

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.lightGreen,
+          accentColor: Colors.deepPurple),
       home: Scaffold(
         appBar: AppBar(
           title: Text('EasyList'),
