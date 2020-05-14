af941ade2180249a27dd2f726d0cb19b4afef77a
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-05-15 00:06:48 +0300

    Adding mode switching

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,35 @@ import './product_manager.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
   final String _startingProduct = 'Food Paradise';
+  bool nightMode = false;
+  IconData mode = Icons.wb_sunny;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          brightness: Brightness.light,
+          brightness: nightMode ? Brightness.light : Brightness.dark,
           primarySwatch: Colors.lightGreen,
           accentColor: Colors.deepPurple),
       home: Scaffold(
         appBar: AppBar(
           title: Text('EasyList'),
+          actions: [
+            IconButton(
+              icon: Icon(!nightMode ? Icons.wb_sunny : Icons.filter_drama),
+              onPressed: () {
+                setState(() => nightMode = !nightMode);
+              },
+            )
+          ],
         ),
         body: ProductManager(startingProduct: _startingProduct),
       ),
