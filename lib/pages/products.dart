b885a9e283fac8d6a3fbbc9a440ceacb3fa55979
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-07-08 13:11:07 +0300

    Adding navigation back batton

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -4,11 +4,20 @@ class ProductsDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Product Details'),
-        ),
-        body: Center(
-          child: Text('No product selected!'),
-        ));
+      appBar: AppBar(
+        title: Text('Product Details'),
+      ),
+      body: Center( child: Column(
+        children: <Widget>[
+          Container(
+            child: Text('Details'),
+          ),
+          RaisedButton(
+            child: Text('Back'),
+            onPressed: () => Navigator.pop(context),
+          ),
+        ],
+      ),),
+    );
   }
 }
