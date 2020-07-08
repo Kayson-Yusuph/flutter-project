953df84594ab3436bca6c8ba54b6e76d2afd0bfd
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-07-08 13:58:39 +0300

    Adding color to button

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -7,17 +7,21 @@ class ProductsDetailsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Product Details'),
       ),
-      body: Center( child: Column(
-        children: <Widget>[
-          Container(
-            child: Text('Details'),
-          ),
-          RaisedButton(
-            child: Text('Back'),
-            onPressed: () => Navigator.pop(context),
-          ),
-        ],
-      ),),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Image.asset('assets/food.jpg'),
+            SizedBox(height: 10.0),
+            Text('Details'),
+            SizedBox(height: 10.0),
+            RaisedButton(
+              color: Theme.of(context).primaryColor,
+              child: Text('Back'),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
