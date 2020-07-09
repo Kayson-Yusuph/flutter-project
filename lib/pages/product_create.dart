5b470a9f7f8eb4567173ba9689b9513db68bbb2a
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-07-09 16:39:20 +0300

    Adding a bottom from  sliding modal

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -4,7 +4,19 @@ class ProductCreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Create Product here!'),
+      child: RaisedButton(
+        color: Theme.of(context).primaryColor,
+        child: Text('save'),
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return Center(
+                  child: Text('Save product'),
+                );
+              });
+        },
+      ),
     );
   }
 }
