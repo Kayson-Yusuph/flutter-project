9132dde7716042f936b951b2ac6542428c1cd3e5
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-07-28 05:53:55 +0300

    Adding product model

---
+++ lib/models/product.model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Product {
+  String title;
+  double price;
+  String description;
+  String image;
+
+  Product({@required this.title, @required this.price, @required this.description, @required this.image })
+}
