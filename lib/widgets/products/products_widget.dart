81a91b31fd2d04fd995906292f852a82b7cc6150
Kayson Yusuph <yusuphkayxn@gmail.com>
2020-08-01 06:28:41 +0300

    Adding scoped model to the products widget

--- lib/widgets/products/products_widget.dart
+++ lib/widgets/products/products_widget.dart
@@ -1,40 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 import './product_card.dart';
 import '../../models/product.model.dart';
+import '../../scoped-model/products.dart';
 
 class ProductsWidget extends StatefulWidget {
-  final List<Product> products;
-  ProductsWidget(this.products);
   @override
   State<StatefulWidget> createState() => _ProductsWidgetState();
 }
 
 class _ProductsWidgetState extends State<ProductsWidget> {
-  List<Product> _products = [];
-
-  @override
-  void initState() {
-    _products = widget.products;
-    super.initState();
+  _buildProductCard(BuildContext context, List<Product> products) {
+    Widget productCards = Center(
+      child: Text('No product found, please add some'),
+    );
+    if (products.length > 0) {
+      productCards = ListView.builder(
+        itemBuilder: (BuildContext context, int index) => ProductCard(
+          index: index,
+          product: products[index],
+        ),
+        itemCount: products.length,
+      );
+    }
+    return productCards;
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget productCards;
-    if (_products.length > 0) {
-      productCards = ListView.builder(
-        itemBuilder: (BuildContext context, int index) => ProductCard(
-          index: index,
-          product: _products[index],
-        ),
-        itemCount: _products.length,
-      );
-    } else {
-      productCards = Center(
-        child: Text('No product found, please add some'),
-      );
-    }
-    return productCards;
+    return ScopedModelDescendant<ProductsModel>(
+        builder: (BuildContext context, Widget child, ProductsModel model) {
+      return _buildProductCard(context, model.products);
+    });
   }
 }
